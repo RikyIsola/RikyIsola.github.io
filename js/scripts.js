ba22977b0c791780a591dd0d04dc23e75132bfbc
rikyisola <riky.isola@gmail.com>
2018-03-23 16:18:55 +0100

    modify

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var language=navigator.language||navigator.userLanguage;
+if(language=="it-IT")
+{
+	document.getElementById("contact").innerHTML="CONTATTI";
+	document.getElementById("home").innerHTML="HOME";
+	document.getElementById("about").innerHTML="INFO";
+}
