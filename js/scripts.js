3be26585bd646297b6b3eeb9cd0cb854e2dd26f6
RikyIsola <riky.isola@gmail.com>
2018-03-24 08:38:00 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var language=navigator.language||navigator.userLanguage;
-if(language=="it-IT")
+document.it=language=="it-IT";
+if(document.it)
 {
 	document.getElementById("contact").innerHTML="CONTATTI";
 	document.getElementById("home").innerHTML="HOME";
